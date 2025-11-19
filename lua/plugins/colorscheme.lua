3d038070274c245bf15b93b5b0a283aa01ead371
fixxiesedtech2 <fixxiesedtech2@gmail.com>
2025-11-19 16:42:12 +0000

    feat: my very own instance

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,31 @@
+--[[ return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end,
+}
+]]
+
+--[[ {
+	"rebelot/kanagawa.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		require("kanagawa").setup({})
+		vim.cmd("colorscheme kanagawa")
+	end,
+}
+]]
+
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			vim.cmd("colorscheme catppuccin"),
+		})
+	end,
+}
---
+++ lua/plugins/init.lua
@@ -0,0 +1,31 @@
+return {
+	{ "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
+
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
+
+	{
+		"windwp/nvim-ts-autotag",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+			})
+		end,
+	},
+}
